d83678fa32ebab433a0349b1fc03b0676421ef82
Fransuelton <elton6103@gmail.com>
2024-02-28 06:58:58 -0300

    fix: update profile image link

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,7 +3,7 @@
 <template>
   <section class="profile">
     <img
-      src="https://media.licdn.com/dms/image/D4D03AQEk2Bf43_zSiw/profile-displayphoto-shrink_800_800/0/1701164167273?e=1706745600&v=beta&t=Ww60f9npTGUYcrd2-T92h3JzqF_oz76r43B8wWZRtFM"
+      src="https://media.licdn.com/dms/image/D4D03AQEk2Bf43_zSiw/profile-displayphoto-shrink_800_800/0/1701164167273?e=1713398400&v=beta&t=DYbA_kgcORSFOKjj1XW4nt4Kb5z0bRDBj6_QZLoaACk"
       alt="Foto de Fransuelton, sorrindo, usando terno azul e fundo branco." />
     <h1>Fransuelton</h1>
     <h2>Desenvolvedor Front-End</h2>
