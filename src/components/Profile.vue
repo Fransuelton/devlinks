b8e038ca3923fa7998fca25b935cfbb7c14f436e
Fransuelton <elton6103@gmail.com>
2024-03-02 07:28:38 -0300

    style: change title position

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,9 +4,10 @@
   <section class="profile">
     <img
       src="https://media.licdn.com/dms/image/D4D03AQEk2Bf43_zSiw/profile-displayphoto-shrink_800_800/0/1701164167273?e=1713398400&v=beta&t=DYbA_kgcORSFOKjj1XW4nt4Kb5z0bRDBj6_QZLoaACk"
-      alt="Foto de Fransuelton, sorrindo, usando terno azul e fundo branco." />
+      alt="Foto de Fransuelton, sorrindo, usando terno azul e fundo branco."
+    />
     <h1>Fransuelton</h1>
-    <h2>Desenvolvedor Front-End</h2>
+    <h2>Desenvolvedor de Software</h2>
   </section>
 </template>
 
